d5ae8166e2d8942edae8f8df9418c3487294a32b
Rachael Maphopha <rachael.maphopha@clickatell.com>
2021-06-29 20:54:21 +0200

    Initial commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Product;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $products = Product::all();
+
+        return view('products.index',compact('products'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $products = Product::all();
+        
+        $productJsonData = json_encode($products);
+        if (file_put_contents('products.json', $productJsonData . '|', FILE_APPEND)) {
+            if ($request->ajax()) {
+                return response()->json(['status' => 'OK', 'data' => $products]);
+            } 
+        }
+
+        return view('products.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'quantity' => 'required',
+            'price' => 'required',
+        ]);
+
+        Product::create($request->all());
+
+        return redirect()->route('products.index')->with('success','Product created successfully.');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $product = Product::find($id);
+        return view('products.show',compact('product'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $product = Product::find($id);
+        return view('products.edit',compact('product'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $product = Product::find($id);
+
+        $request->validate([
+            'name' => 'required',
+            'quantity' => 'required',
+            'price' => 'required',
+        ]);
+
+
+        $product->update($request->all());
+
+        return redirect()->route('products.index')->with('success','Product updated successfully');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $product = Product::find($id);
+        $product->delete();
+
+         return redirect()->route('products.index')
+                         ->with('success','product deleted successfully');
+    }
+}
---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,85 @@
+@extends('layout.app')
+@section('content')
+
+    <div class="row" style="margin-top: 3rem;">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h1>Coalition Technologies </h1>
+
+                <form action="{{ route('products.store') }}" method="POST">
+    @csrf
+
+     <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Name:</strong>
+                <input type="text" name="name" class="form-control" placeholder="Title">
+            </div>
+        </div>
+
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Quantity:</strong>
+                <input type="text" name="quantity" class="form-control" placeholder="Quantity">
+            </div>
+        </div>
+
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Price:</strong>
+                <input type="text" name="price" class="form-control" placeholder="Price">
+            </div>
+        </div>
+        
+        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                <button type="submit" class="btn btn-primary">Submit</button>
+        </div>
+    </div>
+</form>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-success" href="{{ route('products.create') }}"> Create New Product</a>
+            </div>
+        </div>
+    </div>
+
+    @if ($message = Session::get('success'))
+        <div class="alert alert-success">
+            <p>{{ $message }}</p>
+        </div>
+    @endif
+
+    <table style="margin-top: 3rem;" class="table table-bordered">
+        <tr>
+            <th>Product name</th>
+            <th>Quantity in stock</th>
+            <th>Price per item</th>
+            <th>Datetime submitted</th>
+            <th>Total value number</th>
+        </tr>
+        @foreach ($products as $product)
+        <tr>
+            <td>{{ $product->name }}</td>
+            <td>{{ $product->quantity }}</td>
+            <td>{{ $product->price }}</td>
+            <td>
+                <form action="{{ route('products.destroy',$product->id) }}" method="POST">
+                    <a class="btn btn-info" href="{{ route('products.show',$product->id) }}">Show</a>
+                    <a class="btn btn-primary" href="{{ route('products.edit',$product->id) }}">Edit</a>
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="btn btn-danger">Delete</button>
+                </form>
+            </td>
+            <td>
+            {{ $product->quantity*$product->price }}
+            </td>
+            
+        </tr>
+        
+        @endforeach
+        <td>Total of all value numbers - <strong></strong></td>
+    </table>
+    
+
+@endsection
